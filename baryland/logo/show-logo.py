d1c2168a98ec67375799b2fe548b0c32dca21f60
malybaryl <kacper_barylowicz@wp.pl>
2026-05-13 01:42:14 +0200

    feat: fastfetch baryland logo

--- baryland/logo/show-logo.py
+++
@@ -1,9 +0,0 @@
-# Source - https://stackoverflow.com/a/70213858
-# Posted by DanzThePure
-# Retrieved 2026-05-12, License - CC BY-SA 4.0
-
-f = open('ascii.txt')
-content = f.read()
-print(content)
-f.close()
-
